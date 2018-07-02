46ae740a1ba17052ce405ec57ea16a0fe5c4ad10
samip584 <samip0884@gmail.com>
2018-07-02 23:29:24 +0545

    Enters the indivisual pokemon link

--- pokedata.py
+++ pokedata.py
@@ -1,10 +1,16 @@
 import re
 from urllib.request import Request, urlopen
 
+
 url = 'http://pokemondb.net/pokedex/national'
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = urlopen(req).read().decode('UTF8')
-
+link = 'https://pokemondb.net/pokedex/'
 poke_names = re.findall(r'<a class="ent-name" href="/pokedex/.*?">(.*?)</a>', html)
-print(poke_names)
 
+poke_url=[]
+for poke_name in poke_names:
+    poke_url = link + poke_name
+    req = Request(poke_url, headers={'User-Agent': 'Mozilla/5.0'})
+    html = urlopen(req).read().decode('UTF8')
+    print(html)
