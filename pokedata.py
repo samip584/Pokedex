89049300da2b33362fb0ab77795bedd1c1059667
prtx <prtx.pratikshrestha@gmail.com>
2018-07-02 22:56:15 +0545

    Pokemon name list using regex

--- pokedata.py
+++ pokedata.py
@@ -1,24 +1,10 @@
+import re
 from urllib.request import Request, urlopen
-lis = []
-pokelis = []
-i = 0
-l = 0
+
 url = 'http://pokemondb.net/pokedex/national'
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = urlopen(req).read().decode('UTF8')
-print(html)
-string = 'ent-name" href="/pokedex/'
-lis.append(html.find(string,0))
-l = lis[i]+len(string)
-while(html[l]!='"'):
-	l += 1
-pokelis.append(html[(lis[i]+len(string)):l])
-while(i != 806):
-	i = i + 1
-	lis.append(html.find(string,lis[i-1]+1))
-	l = lis[i]+len(string)
-	while(html[l]!='"'):
-		l += 1
-	pokelis.append(html[(lis[i]+len(string)):l])
-print(pokelis)
+
+poke_names = re.findall(r'<a class="ent-name" href="/pokedex/.*?">(.*?)</a>', html)
+print(poke_names)
 
