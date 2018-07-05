5e0ce16f96f7f3b73a5f1a37f84134237117515a
prtx <prtx.pratikshrestha@gmail.com>
2018-07-05 22:03:09 +0545

    Minor refactoring

--- pokedata.py
+++ pokedata.py
@@ -2,15 +2,16 @@ import re
 from urllib.request import Request, urlopen
 
 
-url = 'http://pokemondb.net/pokedex/national'
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+pokedex_page_url = 'http://pokemondb.net/pokedex/national'
+pokemon_page_url = 'https://pokemondb.net/pokedex/'
+
+req = Request(pokedex_page_url, headers={'User-Agent': 'Mozilla/5.0'})
 html = urlopen(req).read().decode('UTF8')
-link = 'https://pokemondb.net/pokedex/'
 poke_names = re.findall(r'<a class="ent-name" href="/pokedex/.*?">(.*?)</a>', html)
 
-poke_url=[]
+poke_url = []
 for poke_name in poke_names:
-    poke_url = link + poke_name
+    poke_url = pokemon_page_url + poke_name
     req = Request(poke_url, headers={'User-Agent': 'Mozilla/5.0'})
     html = urlopen(req).read().decode('UTF8')
     print(html)
